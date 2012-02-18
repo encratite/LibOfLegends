01225a59d7ff6989cef683b3b0e3216733020972
OffsetDonor <offsetdonor@gmail.com>
2012-02-18 00:39:08 +0000

    Made the example program a simple command prompt.
    Signed-off-by: OffsetDonor <offsetdonor@gmail.com>

--- LibOfLegendsExample/Program.cs
+++ LibOfLegendsExample/Program.cs
@@ -12,61 +12,192 @@ using LibOfLegends;
 
 namespace LolineFX
 {
-    class GetRecentGamesByNameContext
+    public class ArgumentListException : ApplicationException
     {
-        public GetRecentGamesByNameContext(RPCService service, string name)
+    }
+
+    public class InvalidCommandException : ApplicationException
+    {
+    }
+
+    public class Command
+    {
+        public Command(string command)
+        {
+            string[] parts = command.Split(new string[] {" "}, 2, StringSplitOptions.None);
+
+            if (parts.Length == 0)
+                throw new InvalidCommandException();
+
+            Name = parts[0];
+
+            if (!ExpectedArgs.ContainsKey(Name))
+                throw new InvalidCommandException();
+
+            Arguments = Utility.CommandLineToArgs(parts[1]).ToList();
+            if (Arguments.Count != ExpectedArgs[Name])
+                throw new InvalidCommandException();
+        }
+
+        public Dictionary<string, int> ExpectedArgs = new Dictionary<string, int>()
+        {
+            {"GetSummonerByName", 1},
+            {"GetRecentGames", 1},
+            {"GetRecentGamesByName", 1}
+        };
+
+        public string Name;
+        public List<string> Arguments;
+    }
+
+    #region RPC Contexts
+
+    public abstract class ExecutionContext
+    {
+        public ExecutionContext(RPCService service)
         {
             _service = service;
+        }
+
+        public abstract void Execute();
+
+        protected void _Wait()
+        {
+            lock(this)
+                Monitor.Wait(this);
+        }
+
+        protected void _Signal()
+        {
+            lock (this)
+                Monitor.Pulse(this);
+        }
+
+        protected RPCService _service;
+    }
+
+    public class GetSummonerByNameContext : ExecutionContext
+    {
+        public GetSummonerByNameContext(RPCService service, string name) : base(service)
+        {
             _name = name;
+            AccountID = -1;
         }
 
-        public void Retrieve()
+        public override void Execute()
         {
-            _service.GetSummonerByName(_name, new Responder<object>(OnGetSummonerByName));
+            _service.GetSummonerByName(_name, new Responder<object>(_OnGetSummonerByName));
+            _Wait();
         }
 
-        public void OnGetSummonerByName(object o)
+        private void _OnGetSummonerByName(object o)
         {
-            Console.WriteLine("\t<GetSummonerByName>");
-            try
-            {
-                Dictionary<string, object> dictionary = (Dictionary<string, object>)o;
-                _accountID = Convert.ToInt32(dictionary["acctId"]);
-                _service.GetRecentGames(_accountID, new Responder<object>(OnGetRecentGames));
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("{0}\n{1}", e.Message, e.StackTrace);
-            }
-            Console.WriteLine("\t</GetSummonerByName>");
-        }
-
-        public void OnGetRecentGames(object o)
-        {
-            Console.WriteLine("\tGetRecentGames");
             Dictionary<string, object> dictionary = (Dictionary<string, object>)o;
+            AccountID = Convert.ToInt32(dictionary["acctId"]);
+            _Signal();
+        }
+
+        public override string ToString()
+        {
+            return AccountID.ToString();
+        }
+
+        private string _name;
+        public int AccountID { get; set; }
+    }
+
+    public class GetRecentGamesContext : ExecutionContext
+    {
+        public GetRecentGamesContext(RPCService service, int accountID)
+            : base(service)
+        {
+            _accountID = accountID;
+        }
+
+        public override void Execute()
+        {
+            _service.GetRecentGames(_accountID, new Responder<object>(_OnGetRecentGames));
+            _Wait();
+        }
+
+        private void _OnGetRecentGames(object o)
+        {
+            Dictionary<string, object> dictionary = (Dictionary<string, object>) o;
 
             foreach (object gameStat in (FluorineFx.AMF3.ArrayCollection)dictionary["gameStatistics"])
-                RecentGames.Add((Dictionary<string, object>) gameStat);
+                RecentGames.Add((Dictionary<string, object>)gameStat);
 
             // Convert this to a more strongly typed list and sort it by gameId.
             RecentGames = RecentGames.ConvertAll(i => (Dictionary<string, object>)i);
             RecentGames = RecentGames.OrderBy(g => g["gameId"]).ToList();
 
             // Signal that the games have been retrieved.
-            lock (this)
-                Monitor.Pulse(this);
-
-
-            Console.WriteLine("\t</GetRecentGames>");
+            _Signal();
         }
 
-        public List<Dictionary<string, object>> RecentGames = new List<Dictionary<string,object>>();
+        public override string ToString()
+        {
+            string text = string.Empty;
 
-        private RPCService _service;
-        private string _name;
+            foreach (Dictionary<string, object> d in RecentGames)
+            {
+	            text += "\tNew match\n";
+	            foreach(KeyValuePair<string, object> kvp in d)
+	            {
+	                if (kvp.Value is string || kvp.Value is int || kvp.Value is bool || kvp.Value is double)
+	                    text += string.Format("\t\t{0}, {1}\n", kvp.Key, kvp.Value);
+	            }
+            }
+
+            return text;
+        }
+
+        public List<Dictionary<string, object>> RecentGames = new List<Dictionary<string, object>>();
+
         private int _accountID;
     }
+
+    public class GetRecentGamesByNameContext : ExecutionContext
+    {
+
+        public GetRecentGamesByNameContext(RPCService service, string name) : base(service)
+        {
+            _name = name;
+            _getSummonerByNameContext = null;
+            _getRecentGamesContext = null;
+        }
+
+        public override void Execute()
+        {
+            Console.WriteLine("Getting recent games for {0}", _name);
+
+            _getSummonerByNameContext = new GetSummonerByNameContext(_service, _name);
+            _getSummonerByNameContext.Execute();
+
+            _getRecentGamesContext = new GetRecentGamesContext(_service, _getSummonerByNameContext.AccountID);
+            _getRecentGamesContext.Execute();
+            
+            if (_getRecentGamesContext.RecentGames == null)
+                throw new ApplicationException(string.Format("Error retrieving games for {0}", _name));
+        }
+
+        public List<Dictionary<string, object>> RecentGames
+        {
+            get { return _getRecentGamesContext.RecentGames; }
+        }
+
+        public override string ToString()
+        {
+            return _getRecentGamesContext.ToString();
+        }
+
+        private string _name;
+
+        private GetSummonerByNameContext _getSummonerByNameContext; 
+        private GetRecentGamesContext _getRecentGamesContext;
+    }
+
+    #endregion
 
     class Program
     {
@@ -74,7 +205,7 @@ namespace LolineFX
 
         static void Main(string[] args)
         {
-            _rpc.Connect(args[0], args[1], OnConnect );
+            _rpc.Connect(args[0], args[1], OnConnect);
 
             // Eugh.
             while(true)
@@ -94,32 +225,59 @@ namespace LolineFX
             t.Start();
         }
 
+        static void GetRecentGamesByName(string name)
+        {
+        }
+
         static void PerformQueries()
         {
             while (true)
             {
-                Console.Write("Please input a summoner name: ");
-                string summoner = Console.ReadLine();
+                Console.Write("[Legendary Prompt] ");
+                string command = Console.ReadLine();
 
-                Console.WriteLine("Getting recent games for {0}", summoner);
-                GetRecentGamesByNameContext c = new GetRecentGamesByNameContext(_rpc, summoner);
-                c.Retrieve();
-
-                // Wait for the games to be retrieved.
-                lock (c)
-                    Monitor.Wait(c);
-
-                Console.WriteLine("Printing stuff now.");
-                foreach (Dictionary<string, object> d in c.RecentGames)
+                try
                 {
-                    Console.WriteLine("\tNew match");
-                    foreach(KeyValuePair<string, object> kvp in d)
+
+                    Command c = new Command(command);
+
+                    switch (c.Name)
                     {
-                        if (kvp.Value is string || kvp.Value is int || kvp.Value is bool || kvp.Value is double)
-                            Console.WriteLine("\t\t{0}, {1}", kvp.Key, kvp.Value);
+                        case "GetRecentGamesByName":
+                        {
+                            GetRecentGamesByNameContext games = new GetRecentGamesByNameContext(_rpc, c.Arguments[0]);
+                            games.Execute();
+
+                            Console.WriteLine(games);
+                            break;
+                        }
+                        case "GetRecentGames":
+                        {
+                            GetRecentGamesContext games = new GetRecentGamesContext(_rpc, int.Parse(c.Arguments[0]));
+                            games.Execute();
+
+                            Console.WriteLine(games);
+                            break;
+                        }
+                        case "GetSummonerByName":
+                        {
+                            GetSummonerByNameContext name = new GetSummonerByNameContext(_rpc, c.Arguments[0]);
+                            name.Execute();
+
+                            Console.WriteLine(name);
+                            break;
+                        }
+                        default:
+                        {
+                            Console.WriteLine("Unrecognised command, type ? or help for a list of commands");
+                            break;
+                        }
                     }
                 }
-
+                catch (Exception e)
+                {
+                    Console.WriteLine("Error executing command");
+                }
                 Console.WriteLine();
             }
         }
